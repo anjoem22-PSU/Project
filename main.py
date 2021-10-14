77c35d9b5ba840b2c1e2c35e3df2d617a29ff365
anjoem22-PSU <amm9971@psu.edu>
2021-10-14 10:19:24 -0400

    Basic main loop and beginnings of grid

--- main.py
+++ main.py
@@ -1,1 +1,40 @@
-print("Hello World")
+import pygame
+import sys
+import grid
+
+pygame.init()
+
+size = width,height = 1280,720
+grid_size = gwidth,gheight = 50,50
+
+black       = 0  , 0  , 0
+white       = 255, 255, 255
+enter_color = 0  , 255, 0
+exit_color  = 255, 0  , 0
+s_color     = 255, 100, 100
+p_color     = 255, 0  , 255
+
+screen = pygame.display.set_mode(size)
+
+
+def step():
+    pass
+
+def draw():
+    screen.fill(white)
+    pygame.display.flip()
+
+
+def main():
+    while 1:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+        
+        step()
+        draw()
+
+if __name__ == "__main__":
+    main()
+
+
